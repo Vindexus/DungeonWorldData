90cbf7d97a441e95c2f14a1783fb3ece883056aa
Colin Kierans <kierans@monstercat.com>
2017-01-24 16:45:31 -0800

    Added some more equipment

--- source/equipment/index.js
+++ source/equipment/index.js
@@ -1,10 +1,45 @@
 module.exports = {
-  'ragged_bow': {
+  ragged_bow: {
     name: 'Ragged Bow',
     tags: ['near', '15 coins', '2 weight']
   },
   fine_bow: {
     name: 'Fine Bow',
     tags: ['near', 'far', '60 coins', '2 weight']
+  },
+  crossbow: {
+    name: 'Crossbow',
+    tags: ['near', '+1 damage', 'reload', '35 coins', '3 weight']
+  },
+  bundle_of_arrows: {
+    name: 'Bundle of Arrows',
+    tags: ['3 ammo', '1 coin', '1 weight']
+  },
+  elven_arrows: {
+    name: 'Elven Arrows',
+    tags: ['4 ammo', '20 coins', '1 weight']
+  },
+  club_shillelagh: {
+    name: 'Club, Shillelagh',
+    tags: ['close', '1 coin', '1 weight']
   }
 }
+
+/*
+Fine Bow; near, far, 60 coins, 2 weight
+Hunter’s Bow; near, far, 100 coins, 1 weight
+Crossbow; near, +1 damage, reload, 35 coins, 3 weight
+Bundle of Arrows; 3 ammo, 1 coin, 1 weight
+Elven Arrows; 4 ammo, 20 coins, 1 weight
+Club, Shillelagh; close, 1 coin, 2 weight
+Staff; close, two-handed, 1 coin, 1 weight
+Dagger, Shiv, Knife; hand, 2 coins, 1 weight
+Throwing Dagger; thrown, near, 1 coin, 0 weight
+Short Sword, Axe, Warhammer, Mace; close, 8 coins, 1 weight
+Spear; reach, thrown, near, 5 coins, 1 weight
+Long Sword, Battle Axe, Flail; close, +1 damage, 15 coins, 2 weight
+Halberd; reach, +1 damage, two-handed, 9 coins, 2 weight
+Rapier; close, precise, 25 coins, 1 weight
+Dueling Rapier; close, 1 piercing, precise, 50 coins, 2 weight
+}
+*/
