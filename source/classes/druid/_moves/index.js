3e09ebae50b8aac6a8786a830b18bea2a6a0240f
Colin Kierans <kierans@monstercat.com>
2017-06-01 17:55:06 -0700

    First strike at Druid

---
+++ source/classes/druid/_moves/index.js
@@ -0,0 +1,141 @@
+module.exports = 
+{
+  "born_of_the_soil": {
+    "name": "Born of the Soil",
+    "key": "born_of_the_soil",
+    "description": "You learned your magic in a place whose spirits are strong and ancient and they’ve marked you as one of their own. No matter where you go, they live within you and allow you to take their shape. Choose one of the following. It is the land to which you are attuned—when shapeshifting you may take the shape of any animal who might live in your Land.\n\n - The Great Forests\n - The Whispering Plains\n - The Vast Desert\n - The Stinking Mire\n - The River Delta\n - The Depths of the Earth\n - The Sapphire Islands\n - The Open Sea\n - The Towering Mountains\n - The Frozen North\n - The Blasted Wasteland\nChose a tell—a physical attribute that marks you as born of the soil—that reflects the spirit of your land. It may be an animal feature like antlers or leopard’s spots or something more general: hair like leaves or eyes of glittering crystal. Your tell remains no matter what shape you take."
+  },
+  "by_nature_sustained": {
+    "name": "By Nature Sustained",
+    "key": "by_nature_sustained",
+    "description": "You don’t need to eat or drink. If a move tells you to mark off a ration just ignore it."
+  },
+  "spirit_tongue": {
+    "name": "Spirit Tongue",
+    "key": "spirit_tongue",
+    "description": "The grunts, barks, chirps, and calls of the creatures of the wild are as language to you. You can understand any animal native to your land or akin to one whose essence you have studied."
+  },
+  "shapeshifter": {
+    "name": "Shapeshifter",
+    "key": "shapeshifter",
+    "description": "When you call upon the spirits to change your shape, roll+Wis. ✴On a 10+ hold 3. ✴On a 7–9 hold 2. ✴On a miss hold 1 in addition to whatever the GM says.\nYou may take on the physical form of any species whose essence you have studied or who lives in your land: you and your possessions meld into a perfect copy of the species’ form. You have any innate abilities and weaknesses of the form: claws, wings, gills, breathing water instead of air. You still use your normal stats but some moves may be harder to trigger—a housecat will find it hard to do battle with an ogre. The GM will also tell you one or more moves associated with your new form. Spend 1 hold to make that move. Once you’re out of hold, you return to your natural form. At any time, you may spend all your hold and revert to your natural form."
+  },
+  "studied_essence": {
+    "name": "Studied Essence",
+    "key": "studied_essence",
+    "description": "When you spend time in contemplation of an animal spirit, you may add its species to those you can assume using shapeshifting."
+  },
+  "druid_elf": {
+    "name": "Elf",
+    "key": "druid_elf",
+    "description": "The sap of the elder trees flows within you. In addition to any other attunements, the Great Forest is always considered your land."
+  },
+  "druid_human": {
+    "name": "Human",
+    "key": "druid_human",
+    "description": "As your people learned to bind animals to field and farm, so too are you bound to them. You may always take the shape of any domesticated animal, in addition to your normal options."
+  },
+  "druid_halfling": {
+    "name": "Halfling",
+    "key": "druid_halfling",
+    "description": "You sing the healing songs of spring and brook. When you make camp, you and your allies heal +1d6."
+  },
+  "hunters_brother": {
+    "name": "Hunter’s Brother",
+    "key": "hunters_brother",
+    "description": "Choose one move from the ranger class list."
+  },
+  "red_of_tooth_and_claw": {
+    "name": "Red of Tooth and Claw",
+    "key": "red_of_tooth_and_claw",
+    "description": "When you are in an appropriate animal form (something dangerous) increase your damage to d8."
+  },
+  "communion_of_whispers": {
+    "name": "Communion of Whispers",
+    "key": "communion_of_whispers",
+    "description": "When you spend time in a place, making note of its resident spirits and calling on the spirits of the land, roll+Wis. You will be granted a vision of significance to you, your allies, and the spirits around you. ✴On a 10+ the vision will be clear and helpful to you. ✴On a 7–9 the vision is unclear, its meaning murky. ✴On a miss, the vision is upsetting, frightening, or traumatizing. The GM will describe it. Take -1 forward."
+  },
+  "barkskin": {
+    "name": "Barkskin",
+    "key": "barkskin",
+    "description": "So long as your feet touch the ground you have +1 armor."
+  },
+  "eyes_of_the_tiger": {
+    "name": "Eyes of the Tiger",
+    "key": "eyes_of_the_tiger",
+    "description": "When you mark an animal (with mud, dirt, or blood) you can see through that animal’s eyes as if they were your own, no matter what distance separates you. Only one animal at a time may be marked in this way."
+  },
+  "shed": {
+    "name": "Shed",
+    "key": "shed",
+    "description": "When you take damage while shapeshifted you may choose to revert to your natural form to negate the damage."
+  },
+  "thing_talker": {
+    "name": "Thing-Talker",
+    "key": "thing_talker",
+    "description": "You see the spirits in the sand, the sea and the stone. You may now apply your spirit tongue, shapeshifting and studied essence to inanimate natural objects (plants and rocks) or creatures made thereof, as well as animals. Thing-talker forms can be exact copies or can be mobile vaguely humanoid-shaped entities."
+  },
+  "formcrafter": {
+    "name": "Formcrafter",
+    "key": "formcrafter",
+    "description": "When you shapeshift choose a stat: you take +1 ongoing to rolls using that stat while shifted. The GM will choose a stat, too: you take -1 ongoing to rolls using that stat while shifted."
+  },
+  "elemental_mastery": {
+    "name": "Elemental Mastery",
+    "key": "elemental_mastery",
+    "description": "When you call on the primal spirits of fire, water, earth or air to perform a task for you roll+Wis. ✴On a 10+ choose two. ✴On a 7–9 choose one. ✴On a miss, some catastrophe occurs as a result of your calling.\n\n - The effect you desire comes to pass\n - You avoid paying nature’s price \n - You retain control"
+  },
+  "balance": {
+    "name": "Balance",
+    "key": "balance",
+    "description": "When you deal damage, take 1 balance. When you touch someone and channel the spirits of life you may spend balance. For each balance spent, heal 1d4 HP."
+  },
+  "embracing_no_form": {
+    "name": "Embracing No Form",
+    "key": "embracing_no_form",
+    "description": "When you shapeshift, roll 1d4 and add that total to your hold."
+  },
+  "doppelgängers_dance": {
+    "name": "Doppelgänger’s Dance",
+    "key": "doppelgängers_dance",
+    "description": "You are able to study the essence of specific individuals to take their exact form, including men, elves, or the like. Suppressing your tell is possible, but if you do, take -1 ongoing until you return to your own form."
+  },
+  "blood_and_thunder": {
+    "name": "Blood and Thunder",
+    "key": "blood_and_thunder",
+    "replaces": "red_of_tooth_and_claw",
+    "description": "When you are in an appropriate animal form (something dangerous) increase your damage to d10."
+  },
+  "the_druid_sleep": {
+    "name": "The Druid Sleep",
+    "key": "the_druid_sleep",
+    "description": "When you take this move, the next opportunity that you have safety and time to spend in an appropriate location, you may attune yourself to a new land. This effect occurs only once and the GM will tell you how long it will take and what cost you must pay. From then on, you are considered to be born of the soil in both lands."
+  },
+  "world_talker": {
+    "name": "World-Talker",
+    "key": "world_talker",
+    "requires": "thing_talker",
+    "description": "You see the patterns that make up the fabric of the world. You may now apply your spirit tongue, shapeshifter and studied essence moves to pure elements—fire, water, air and earth."
+  },
+  "stalkers_sister": {
+    "name": "Stalker’s Sister",
+    "key": "stalkers_sister",
+    "description": "Choose one move from the ranger class list."
+  },
+  "formshaper": {
+    "name": "Formshaper",
+    "key": "formshaper",
+    "requires": "formcrafter",
+    "description": "You may increase your armor by 1 or deal an additional +1d4 damage while in an animal form. Choose which when you shapeshift."
+  },
+  "chimera": {
+    "name": "Chimera",
+    "key": "chimera",
+    "description": "When you shapeshift, you may create a merged form of up to three different shapes. You may be a bear with the wings of an eagle and the head of a ram, for example. Each feature will grant you a different move to make. Your chimera form follows the same rules as shapeshifter otherwise. "
+  },
+  "weather_weaver": {
+    "name": "Weather Weaver",
+    "key": "weather_weaver",
+    "description": "When you are under open skies when the sun rises the GM will ask you what the weather will be that day. Tell them whatever you like, it comes to pass."
+  }
+}
---
+++ source/classes/druid/index.js
@@ -0,0 +1,164 @@
+module.exports = 
+{
+  "name": "Druid",
+  "key": "druid",
+  "damage": "d6",
+  "base_hp": 6,
+  "load": 6,
+  "description": "Cast your eyes around the fire. What has brought you to these people, stinking of the dust and sweat of the city? Perhaps it is a kindness—do you protect them as the mother bear watches over her cubs? Are they your pack, now? Strange brothers and sisters you have. Whatever your inspiration, they would certainly fail without your sharp senses and sharper claws.\n\nYou are of the sacred spaces; you are born of soil and wear the marks of her spirits on your skin. You may have had a life before, maybe you were a city dweller like them, but not now. You’ve given up that static shape. Listen to your allies pray to their carved stone gods and polish their silver shells. They speak of the glory they’ll find back in that festering town you left behind.\n\nTheir gods are children, their steel is false protection. You walk the old ways, you wear the pelts of the earth itself. You’ll take your share of the treasure, but will you ever walk as one of them? Only time will tell.",
+  "names": {
+    "elf": [
+      "Hycorax",
+      "Ethanwe",
+      "Sinathel",
+      "Demanor",
+      "Menoliir",
+      "Mithralan",
+      "Taeros",
+      "Aegor"
+    ],
+    "halfling": [
+      "Tanner",
+      "Dunstan",
+      "Rose",
+      "Ivy",
+      "Robard",
+      "Mab",
+      "Thistle",
+      "Puck",
+      "Anne",
+      "Serah"
+    ],
+    "human": [
+      "Elana",
+      "Obelis",
+      "Herran",
+      "Syla",
+      "Andanna",
+      "Siobhan",
+      "Aziz",
+      "Pelin",
+      "Sibel",
+      "Nils",
+      "Wei"
+    ]
+  },
+  "bonds": [
+    "{{blank}} smells more like prey than a hunter.",
+    "The spirits spoke to me of a great danger that follows {{blank}}.",
+    "I have showed {{blank}} a secret rite of the Land.",
+    "{{blank}} has tasted my blood and I theirs. We are bound by it."
+  ],
+  "looks": [
+    [
+      "Wise Eyes",
+      "Wild Eyes",
+      "Haunting Eyes"
+    ],
+    [
+      "Furry Hood",
+      "Messy Hair",
+      "Braided Hair"
+    ],
+    [
+      "Ceremonial Garb",
+      "Practical Leathers",
+      "Weathered Hides"
+    ]
+  ],
+  "alignments": {
+    "chaotic": {
+      "description": "Destroy a symbol of civilization.",
+      "name": "Chaotic"
+    },
+    "good": {
+      "description": "Help something or someone grow.",
+      "name": "Good"
+    },
+    "neutral": {
+      "description": "Eliminate an unnatural menace.",
+      "name": "Neutral"
+    }
+  },
+  "alignments_list": {
+    "points_to": "classes.druid.alignments",
+    "list": [
+      "chaotic",
+      "good",
+      "neutral"
+    ]
+  },
+  "gear_choices": [
+    {
+      "label": "",
+      "list": [
+        "hide_armor",
+        "wooden_shield"
+      ]
+    },
+    {
+      "label": "Choose your armament:",
+      "list": [
+        "shillelagh",
+        "staff",
+        "spear"
+      ]
+    },
+    {
+      "label": "Choose one:",
+      "list": [
+        "adventuring_gear",
+        "poultices_and_herbs",
+        "halfling_pipeleaf",
+        "3_antitoxin"
+      ]
+    }
+  ],
+  "starting_moves": {
+    "points_to": "moves",
+    "list": [
+      "born_of_the_soil",
+      "by_nature_sustained",
+      "spirit_tongue",
+      "shapeshifter",
+      "studied_essence"
+    ]
+  },
+  "race_moves": {
+    "points_to": "moves",
+    "list": [
+      "druid_elf",
+      "druid_human",
+      "druid_halfling"
+    ]
+  },
+  "advanced_moves_1": {
+    "points_to": "moves",
+    "list": [
+      "hunters_brother",
+      "red_of_tooth_and_claw",
+      "communion_of_whispers",
+      "barkskin",
+      "eyes_of_the_tiger",
+      "shed",
+      "thing_talker",
+      "formcrafter",
+      "elemental_mastery",
+      "balance"
+    ]
+  },
+  "advanced_moves_2": {
+    "points_to": "moves",
+    "list": [
+      "embracing_no_form",
+      "doppelgängers_dance",
+      "blood_and_thunder",
+      "the_druid_sleep",
+      "world_talker",
+      "stalkers_sister",
+      "formshaper",
+      "chimera",
+      "weather_weaver"
+    ]
+  }
+}
